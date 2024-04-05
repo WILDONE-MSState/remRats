65e33e1de2e0cd5ee86834b04213a0b4575e5e81
Fer Arce <farcego@gmail.com>
2024-04-05 14:58:06 -0500

    added some help file

--- R/simPop.R
+++ R/simPop.R
@@ -1,19 +1,24 @@
 ##' .. content for \description{} (no empty lines) ..
 ##'
-##' .. content for \details{} ..
-##' @title 
+##' simpop generates population realizatiions of indivisduals to be removed. Each realization is obtained by extracting a random value from a distribution Poisson with parameter lambda = Population size. Predefined values set up to 0, forcing users to actually specify both n and N
+##' @title simPop
 ##' @param pops Numer of populations to replicate simulations
-##' @param N population size
+##' @param N Expected population population size
 ##' @param spatial 
 ##' @return 
 ##' @author Fer Arce
-simPop <- function(pops = 1, N, spatial = FALSE){
+simPop <- function(pops = 0, N = 0, spatial = FALSE){
+    if (pops == 0)
+        return(cat('no population requested to simulate'))
     if (spatial == FALSE) {
         x = rpois(n = pops, lambda = N)
-        inds <- data.frame(ID = paste0('IND_ ', 1:x), X = rep(1,x))
-        class(inds) <- 'simPop'
-        ## anyadir de atributo los Ls, units etc
+        if (length(x) > 0){
+            inds <- data.frame(ID = paste0('IND_ ', 1:x), X = rep(1,x))
+            class(inds) <- 'simPop'
+            return(inds)
+        } else {
+            return(cat('Simulated population of 0 individuals \nPlease try again with actual param values\n'))
+        }
     }
-    return(inds)
 }
 
