384a29f5c13e8c5515d97f8f9d9c0a7cec908d2f
Fer Arce <farcego@gmail.com>
2024-04-12 14:16:13 -0500

    Files created

---
+++ R/remSim.R
@@ -0,0 +1,16 @@
+##' function to simulate simple removal datasets
+##'
+##' it needs somehow to stop at zero and fill with zeroes after all individuals have been removed
+##' @title 
+##' @param N population size
+##' @param p capturability
+##' @param j number of occasions
+##' @return 
+##' @author Fer Arce
+remSim<-function(N,p,j){
+    Capt<-numeric(j)
+    for (i in 1:j){
+        Capt[i]<-sum(rbinom(N-sum(Capt),1,prob=p))
+    }
+    return(Capt)
+}
---
+++ test.R
@@ -0,0 +1,6 @@
+source('R/remSim.R')
+source('R/genHistRMark.R')
+source('R/removeMLE.R')
+pob1<-remSim(N=100,p=.3,j=5)
+simudata<-genHistRMark(5,pob1) 
+pop.res1<-removeMLE(simudata)
