58b8fe719731eaff4f0f62efd4ad3986ea4f2903
Fer Arce <farcego.pi@gmail.com>
2024-04-13 11:50:41 -0500

    remSim functional

--- R/remSim.R
+++ R/remSim.R
@@ -1,11 +1,13 @@
-##' function to simulate simple removal datasets
+##' Simple function to simulate removal datasets
 ##'
 ##' it needs somehow to stop at zero and fill with zeroes after all individuals have been removed
-##' @title 
-##' @param N population size
-##' @param p capturability
+##' @title Removal simulation
+##' @param N population size (initial)
+##' @param p capturability (probabilidad of caopture, equal for each individual, in the interval (0,1))
 ##' @param j number of removal occasions
-##' @return
+##' @return a vector of total number of captures per removal event
+##' @examples
+##' simEx <- remSim(100, 0.5, 4)
 ##' @author Fer Arce
 remSim<-function(N,p,j){
     Capt<-numeric(j)
