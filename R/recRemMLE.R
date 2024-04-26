32b38f338552cbe16395019aa9c5646c7b4c8d67
Fer Arce <farcego.pi@gmail.com>
2024-04-26 10:21:37 -0500

    formals added to recRemMLE

--- R/recRemMLE.R
+++ R/recRemMLE.R
@@ -7,22 +7,39 @@
 ##' @param recursive 
 ##' @return 
 ##' @author Fer Arce
-recRemMLE <- function(pob, events, recursive = TRUE){
-    if (events > length(pob)){
+recRemMLE <- function(Data, events, recursive = TRUE){
+    stopifnot(class(Data) == "HistRMarkLong")
+    if (events > length(Data[[2]])){
         ev <- events
-        events <- length(pob)
+        events <- length(Data[[2]])
         warning('\nNumber of requested events (',ev,') is too large for the data\nsupplied. It has been adjusted to its maximum possible \nvalue (',events,').\n\n')
     }
+    pob1 <- Data[[2]]
     out <- vector('list', events)
     for (i in 2:events){
         tmpSim <- genHistRMark(pob1, i, msg = FALSE)
-        tmpSim <- tmpSim[tmpSim$ch != paste(rep(0,times =i), sep = '', collapse = ''), ]
+        empties <- paste(rep(0,times =i), sep = '', collapse = '')
+        kept <- tmpSim[[1]]$ch[tmpSim[[1]]$ch != empties]
+        tmpSim[[1]] <- data.frame(ch = kept, ind = 1)
         tmpPop<-removeMLE(tmpSim)
-        tmpPop[[1]]$n.occ <- i
-        out[i] <- tmpPop
+        tmpPop[[2]][[1]]$n.occ <- i
+        out[i] <- tmpPop$result
     }
     res <- do.call(rbind, out)
     if (any(res$se == 0))
         warning('\nSome of the fitted values are singular and must be ignored.\nYou can identify them by looking to the reported standard errors.\n')
-    return(res)
+    out <- list(data = Data, Nest = res)
+    class(out) <- 'fittedRemMLERec'
+    return(out)
+}
+
+
+print.fittedRemMLERec <- function(object){
+    cat('\nEvolution of the Population size estimate\nat the start of the removal experiment:\n')
+    print(object[[2]])
+}
+
+
+as.data.frame.fittedRemMLE <- function(object){
+    return(object[[2]][[1]])
 }
