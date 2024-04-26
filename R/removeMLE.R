1acef0bdda47d57b159efa509cd0339207ff1318
Fer Arce <farcego.pi@gmail.com>
2024-04-26 13:40:31 -0500

    formal call to RMark::mark

--- R/removeMLE.R
+++ R/removeMLE.R
@@ -11,7 +11,6 @@
 ##' @author Fer Arce
 removeMLE <- function(Data, engine = NULL, model = NULL){
     stopifnot(class(Data) == "HistRMarkLong")
-    require(RMark)
     c.1 = list(formula=~1, fixed = 0)
     if(.Platform$OS.type == 'windows'){
         File <- 'nul'
@@ -20,12 +19,39 @@ removeMLE <- function(Data, engine = NULL, model = NULL){
     }
     
     sink(file = File)
-    data.mark <- data.frame(ch = Data[[1]]$ch, ind = Data[[1]]$ch)
-    ren<-mark(data.mark,model="Closed",model.parameters=list(c=c.1),delete=TRUE, hessian = TRUE, silent = TRUE)
+    data.mark <- data.frame(ch = Data[[1]]$ch, ind = 1)
+    ren<-RMark::mark(data.mark,model="Closed",model.parameters=list(c=c.1),delete=TRUE, hessian = TRUE, silent = TRUE)
     sink()
     N <- ren$results$derived
     ## anyadir una class chula aqui
     output <- list(data = Data, result = N)
     class(output) <- 'fittedRemMLE'
     return(output)
+}
+
+
+
+
+
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param object 
+##' @return 
+##' @author Fer Arce
+print.fittedRemMLE <- function(object){
+    cat('\nPopulation size at the start of the\nremoval experiment:\n')
+    print(object$result$`N Population Size`)
+}
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param object 
+##' @return 
+##' @author Fer Arce
+as.data.frame.fittedRemMLE <- function(object){
+    return(object[[2]][[1]])
 }
