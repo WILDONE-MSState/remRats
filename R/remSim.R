42ea30026fd141352d6027c373b0f5be4aa0ec16
Fer Arce <farcego@gmail.com>
2024-05-04 10:53:35 -0500

    remSim now accepts trap-saturation

--- R/remSim.R
+++ R/remSim.R
@@ -1,18 +1,21 @@
 ##' Function to simulate removal datasets
 ##'
-##' it needs somehow to stop at zero and fill with zeroes after all individuals have been removed
-##' @title Removal simulation
-##' @param N population size (initial)
-##' @param p capturability (probabilidad of caopture, equal for each individual, in the interval (0,1))
-##' @param j number of removal occasions
+##' Simple simulation of removal experiments under basic constraints, following a binomial secuence of removals, with the posibility of adding a cap up to simulate experiments where trap saturation occur.
+##' @title simple removal simulation
+##' @param N Initial population size
+##' @param p capturability (probabilidad of copture, equal for each individual, in the interval (0,1))
+##' @param j number of removal events
+##' @param max.capt maximum number of individuals potentially removed per event (i.e number of single catch traps)
 ##' @return a vector of total number of captures per removal event
 ##' @examples
-##' simEx <- remSim(100, 0.5, 4)
+##' simEx <- remSim(100, 0.5, 4, 50)
 ##' @author Fer Arce
-remSim<-function(N,p,j){
+##' @export
+remSim<-function(N,p,j, max.capt){
     Capt<-numeric(j)
     for (i in 1:j){
-        Capt[i]<-sum(rbinom(N-sum(Capt),1,prob=p))
+        trial <- sum(rbinom(N-sum(Capt),1,prob=p))
+        Capt[i]<- ifelse(trial > max.capt, max.capt, trial)
     }
     return(Capt)
 }
