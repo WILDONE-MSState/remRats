1831ee90910d46c65fe97b0bda229db5f95bd6cb
Fer Arce <farcego@gmail.com>
2024-05-09 09:50:11 -0500

    remSim finished for first release

--- R/remSim.R
+++ R/remSim.R
@@ -1,6 +1,6 @@
 ##' Function to simulate removal datasets
 ##'
-##' Simple simulation of removal experiments under basic constraints, following a binomial secuence of removals, with the posibility of adding a cap up to simulate experiments where trap saturation occur.
+##' Simple simulation of removal experiments under basic constraints, following a binomial secuence of removals, with the posibility of adding a cap up to simulate experiments where trap saturation occur. To prevent this behavior to occur, max.capt should be fixed to a ridiculously unrealistic large value. This function does not allow to simulate populations  with time or individual-level varying parameters. It is defined and mantained for the purpose of simple checking and large simulations.
 ##' @title simple removal simulation
 ##' @param N Initial population size
 ##' @param p capturability (probabilidad of copture, equal for each individual, in the interval (0,1))
