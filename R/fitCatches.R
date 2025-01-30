c5c66e01cc61f8d0f15baff9c63e6a492af2f20c
Fer Arce <farcego@gmail.com>
2025-01-30 12:10:11 -0600

    function fixed and help file added

--- R/fitCatches.R
+++ R/fitCatches.R
@@ -1,17 +1,26 @@
-##' .. content for \description{} (no empty lines) ..
+##' Function that defines the gam structured used.
 ##'
-##' .. content for \details{} ..
-##' @title Fitting a gam model to cumulative numbers of removed individuals.
-##' @param Data
-##' @param k smoothing parameter - number of knots (It cannot be larger than n-1 occasions)
-##' @return 
+##' This function fits a gam to a processed object. The object of this
+##' function is to define the gam structure (mgvc model) over the
+##' cumulative number of animals removed, aimed to smoith a curve with
+##' will be projected in order to evakluate current and future
+##' performance of the removal experiment in regards the aims of the
+##' project. This function is not expected to be called by the end user
+##' @title Fitting a gam model to cumulative numbers of removed
+##'     individuals.
+##' @param Data an object of class fittedRemMLERec
+##' @param k smoothing parameterfot the gam function - number of knots
+##'     (It cannot be larger than n-1 occasions)
+##' @return the object of a fitted gam model.
 ##' @author Fer Arce
-##' @export
-fitCatches <- function(Data, k = 5){
-    capts <- cumsum(pob1)
-    n <- 1:length(pob1)
+##' @noRd
+fitCatches <- function(Data, k = 5) {
+    capts <- as.vector(Data$Nraw$Ncumu)
+    n <- 1:length(capts)
     if (k >= (length(capts) - 1))
         k = length(capts) - 1
-    model <- gam(capts~s(n),bs = 'cr', k = k)
+    model <- gam(capts~s(n, k = k), bs = 'cr', method = 'REML')
+    ## model <- gam(capts~s(n),bs = 'cr', k = k)
+    ## class(model) <- 'fitC'
     return(model)
 }
