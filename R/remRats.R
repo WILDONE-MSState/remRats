90e77cbf90266dda5f1d2b0637eec42f6115e634
Fer Arce <farcego@gmail.com>
2025-02-10 08:50:25 -0600

    wrapper fuinction added

---
+++ R/remRats.R
@@ -0,0 +1,14 @@
+##' Wrapper function to make a full process of removal data using a single function call
+##'
+##' This is a wrapping function to facilitaste the visualization of the analytical procedures implemented in remRats
+##' @title remRats
+##' @param pob1 a numeric vector contianing the number of individuals removed during a remioval experiment at each event. 
+##' @return 
+##' @author Fer Arce
+##' @export
+remRats <- function(pob1){
+    simudata<-genHistRMark(pob1)
+    popEst<-recRemMLE(simudata, events = 5)
+    popEst$Nest[2:5] <- round(popEst$Nest[2:5], 0) 
+    return(popEst)
+}
