9f455fa0f772f7d19f50a07a550d3e7767483a9d
Fer Arce <farcego@gmail.com>
2025-02-10 08:13:47 -0600

    exported

--- R/sinyView.R
+++ R/sinyView.R
@@ -5,22 +5,23 @@
 ##' @param popEst 
 ##' @return open a shiny app in a web browser
 ##' @author Fer Arce
+##' @export
 shinyView <- function(popEst){
-# Define UI
-ui <- shiny::fluidPage(
-    shiny::titlePanel("summary of Removal Trapping Experiment"),
-    shiny::tags$div(
-             style = 'max-width: 800px; margin: auto;',
-             shiny::tabsetPanel(
-                 shiny::tabPanel("Raw data", DT::dataTableOutput("table1")),
-                 shiny::tabPanel("Population estimate", DT::dataTableOutput("table2")),
-                 shiny::tabPanel("% of population trapped", DT::dataTableOutput("table3"))
-             )
-         )
-)
-
-
-
+                                        # Define UI
+    ui <- shiny::fluidPage(
+                     shiny::titlePanel("summary of Removal Trapping Experiment"),
+                     shiny::tags$div(
+                                     style = 'max-width: 800px; margin: auto;',
+                                     shiny::tabsetPanel(
+                                                shiny::tabPanel("Raw data", DT::dataTableOutput("table1")),
+                                                shiny::tabPanel("Population estimate", DT::dataTableOutput("table2")),
+                                                shiny::tabPanel("% of population trapped", DT::dataTableOutput("table3"))
+                                            )
+                                 )
+                 )
+    
+    
+    
     server <- function(input, output, session) {
         output$table1 <- DT::renderDataTable({
             popEst$Nraw
@@ -36,5 +37,6 @@ ui <- shiny::fluidPage(
     
     shiny::shinyApp(ui = ui, server = server)
 }
+
 
 ##shinyView(popEst)
