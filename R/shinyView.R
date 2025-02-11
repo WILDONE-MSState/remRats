95310e5b18357498139226a9ef9fea326c46e43f
Fer Arce <farcego@gmail.com>
2025-02-10 19:23:01 -0600

    shiny app function updated

--- R/shinyView.R
+++ R/shinyView.R
@@ -1,8 +1,8 @@
 ##' Function to visualice the outcome of a remRats analysis
 ##'
-##' This function open a shiny app where the different output tables recursively generated are shown.
+##' This function open a shiny app where the different output tables and time series plots generated are shown.
 ##' @title shinyView
-##' @param popEst 
+##' @param popEst an object of class ****
 ##' @return open a shiny app in a web browser with the data analysis
 ##' @author Fer Arce
 ##' @export
@@ -28,102 +28,51 @@ shinyView <- function(popEst) {
       )
     )
   )
-  
-    
-  
+
   server <- function(input, output, session) {
-    # Raw data tab
-    output$table1 <- DT::renderDataTable({
-    })
-    output$plot1 <- renderPlot({
-      df <- popEst$Nraw
-      if(all(c("n.occ", "Ncumu") %in% names(df))) {
+                                        ## Raw data tab
+      output$table1 <- DT::renderDataTable({
+          DT::datatable(popEst$Nraw, colnames = c('Trapping occasion', 'Captures per occasion', 'Captures (cumulative)'))
+      })
+      output$plot1 <- renderPlot({
+        df <- popEst$Nraw
         ggplot(df, aes(x = n.occ, y = Ncumu)) +
-          geom_point(color = "blue", size = 3) +
-          geom_line(color = "blue") +
-          labs(x = "n.occ", y = "Ncumu", title = "n.occ vs Ncumu (Raw Data)") +
-          theme_minimal()
-      } else {
-        # If the required columns are missing, show a message
-        plot.new()
-        text(0.5, 0.5, "Columns 'n.occ' and/or 'Ncumu' not available", cex = 1.5)
-      }
+            geom_point(color = "blue", size = 3) +
+            geom_line(color = "blue") +
+            labs(x = "Trapping occasion", y = "Cumulative number of Catches") +
+            theme_minimal() +
+            theme(axis.title = element_text(size = rel(2)))
     })
     
     # Population estimate tab
     output$table2 <- DT::renderDataTable({
-      popEst$Nest
+      DT::datatable(popEst$Nprops, colnames = c("Trapping occasion", "Population size estimate", "Standard error", "% (Lower ci)", "% (upper ci)"))
     })
     output$plot2 <- renderPlot({
       df <- popEst$Nest
-      if(all(c("n.occ", "Nestimate") %in% names(df))) {
-        ggplot(df, aes(x = n.occ, y = Nestimate)) +
+      ggplot(df, aes(x = n.occ, y = Nestimate)) +
+                        geom_ribbon(aes(ymin = Nlcl, ymax = Nucl), fill = "lightblue", alpha = 0.3) +
           geom_point(color = "darkgreen", size = 3) +
           geom_line(color = "darkgreen") +
-          labs(x = "n.occ", y = "Nestimate", title = "n.occ vs Ncumu (Population Estimate)") +
-          theme_minimal()
-      } else {
-        plot.new()
-        text(0.5, 0.5, "Columns 'n.occ' and/or 'Ncumu' not available", cex = 1.5)
-      }
+          labs(x = "Trapping occasion", y = "Population estimate") +
+            theme_minimal() +
+            theme(axis.title = element_text(size = rel(2)))
     })
     # % of population trapped tab
     output$table3 <- DT::renderDataTable({
       DT::datatable(popEst$Nprops, colnames = c("Trapping occasion", "Cumulative number of individuals", "% of the population sampled", "% (Lower ci)", "% (upper ci)"))
-
     })
     output$plot3 <- renderPlot({
       df <- popEst$Nprops
-      if(all(c("n.occ", "Pestimate") %in% names(df))) {
-        ggplot(df, aes(x = n.occ, y = Pestimate)) +
+      ggplot(df, aes(x = n.occ, y = Pestimate)) +
+                                  geom_ribbon(aes(ymin = Plcl, ymax = Pucl), fill = "lightblue", alpha = 0.3) +
           geom_point(color = "purple", size = 3) +
           geom_line(color = "purple") +
-          labs(x = "n.occ", y = "Pestimate", title = "n.occ vs Ncumu (% of population trapped)") +
-          theme_minimal()
-      } else {
-        plot.new()
-        text(0.5, 0.5, "Columns 'n.occ' and/or 'Ncumu' not available", cex = 1.5)
-      }
+          labs(x = "Trapping occasion", y = "% of the population Sampled") +
+            theme_minimal() +
+            theme(axis.title = element_text(size = rel(2)))
     })
   }
   
   shinyApp(ui = ui, server = server)
 }
-
-
-
-## shinyView <- function(popEst){
-##                                         # Define UI
-##     ui <- fluidPage(
-##                      titlePanel("summary of Removal Trapping Experiment"),
-##                      tags$div(
-##                                      style = 'max-width: 800px; margin: auto;',
-##                                      tabsetPanel(
-##                                                 tabPanel("Raw data",
-##                                                                 DT::dataTableOutput("table1")),
-##                                                 tabPanel("Population estimate",
-##                                                                 DT::dataTableOutput("table2")),
-##                                                 tabPanel("% of population trapped",
-##                                                                 DT::dataTableOutput("table3"))
-##                                             )
-##                                  )
-##                  )
-    
-    
-    
-##     server <- function(input, output, session) {
-##         output$table1 <- DT::renderDataTable({
-##             popEst$Nraw
-##         })  
-##         output$table2 <- DT::renderDataTable({
-##             popEst$Nest
-##         })
-##         output$table3 <- DT::renderDataTable({
-##             popEst$Nprops
-##         })
-##     }
-    
-    
-##     shinyApp(ui = ui, server = server)
-## }
-
