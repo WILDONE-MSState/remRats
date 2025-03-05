cb673d52f84afbe49ec1982e7f9c5ffd761f331b
Fer Arce <farcego@gmail.com>
2025-03-05 09:27:31 -0600

    projCatches updated

--- R/projCatches.R
+++ R/projCatches.R
@@ -4,7 +4,7 @@
 ##' .. content for \details{} ..
 ##' @title 
 ##' @param Data an object of class 'fittedRemMLERec'.
-##' @param proj numbers of future days to be projected 
+##' @param proj numbers of future days to be projected. 
 ##' @param ... further arguments to be imposed to fitCatches
 ##' @return 
 ##' @author Fer Arce
