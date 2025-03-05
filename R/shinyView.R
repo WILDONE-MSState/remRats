6f21cb607574ee75decbb3963837ca9230c50259
Fer Arce <farcego@gmail.com>
2025-03-05 09:35:53 -0600

    ShinyView now includes capture projections

--- R/shinyView.R
+++ R/shinyView.R
@@ -1,9 +1,9 @@
 ##' Function to visualice the outcome of a remRats analysis
 ##'
-##' This function open a shiny app where the different output tables and time series plots generated are shown.
+##' This function open a shiny app where the different output tables and time series plots generated are shown. This Shiny app is interactive so the end user can decider project or no the model into nearby future to avaluate the expectations of future field effort.
 ##' @title shinyView
-##' @param popEst an object of class ****
-##' @return open a shiny app in a web browser with the data analysis
+##' @param popEst an object of class 'fittedRemMLERec'.
+##' @return open a shiny app in a web browser to visualize the data analysis and projections
 ##' @author Fer Arce
 ##' @export
 shinyView <- function(popEst) {
@@ -16,15 +16,22 @@ shinyView <- function(popEst) {
         tabPanel("Raw data",
                  DT::dataTableOutput("table1"),
                  plotOutput("plot1")
-        ),
+                 ),
         tabPanel("Population estimate",
                  DT::dataTableOutput("table2"),
                  plotOutput("plot2")
-        ),
+                 ),
         tabPanel("% of population trapped",
                  DT::dataTableOutput("table3"),
                  plotOutput("plot3")
-        )
+                 ),
+        tabPanel("Population projection",
+                 numericInput("num_input", "Select a number (0 to 3):", value = 0, min = 0, max = 3),
+                 actionButton("run_btn", "Run"),
+                 textOutput("result"),
+                 DT::dataTableOutput("table4"),
+                 plotOutput("plot4")
+                 )
       )
     )
   )
@@ -58,7 +65,7 @@ shinyView <- function(popEst) {
             theme_minimal() +
             theme(axis.title = element_text(size = rel(2)))
     })
-    # % of population trapped tab
+      ## % of population trapped tab
     output$table3 <- DT::renderDataTable({
       DT::datatable(popEst$Nprops, colnames = c("Trapping occasion", "Cumulative number of individuals", "% of the population sampled", "% (Lower ci)", "% (upper ci)"))
     })
@@ -72,7 +79,29 @@ shinyView <- function(popEst) {
             theme_minimal() +
             theme(axis.title = element_text(size = rel(2)))
     })
+      ## Population proejction
+      observeEvent(input$run_btn, {
+                                        # Pass the selected number to the external function
+          result <- projCatches(popEst, input$num_input)
+          ## output$result <- renderText({
+          ##     paste("Result of Projection: ", result)
+          ## })
+          output$table4 <- DT::renderDataTable({
+              DT::datatable(result, colnames = c("projection","s.e "))
+          })     
+          output$plot4 <- renderPlot({
+              df <- result
+              ggplot(df, aes(x = n.occ, y = fit)) +
+                  geom_ribbon(aes(ymin = lcl, ymax = ucl), fill = "lightblue", alpha = 0.3) +
+                  geom_point(color = "purple", size = 3) +
+                  geom_line(color = "purple") +
+                  labs(x = "Trapping occasion", y = "% of the population Sampled") +
+                  theme_minimal() +
+                  theme(axis.title = element_text(size = rel(2)))
+          })
+      })
+      
   }
-  
-  shinyApp(ui = ui, server = server)
+    
+    shinyApp(ui = ui, server = server)
 }
