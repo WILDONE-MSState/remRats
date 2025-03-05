45f73989ac22bf7c7146d77473ba523a03bbbcc7
Fer Arce <farcego@gmail.com>
2025-03-05 09:59:58 -0600

    help file updated

--- R/projCatches.R
+++ R/projCatches.R
@@ -1,11 +1,11 @@
 
-##' Function to fit and project a gam curve.
+##' Function to fit and project a gam curve to the cumulative number of individuals removed.
 ##'
-##' .. content for \details{} ..
+##' This function fits a gam model to the provided number of cumulative captures, and then project the model fit to near future trapping events
 ##' @title 
 ##' @param Data an object of class 'fittedRemMLERec'.
 ##' @param proj numbers of future days to be projected. 
-##' @param ... further arguments to be imposed to fitCatches
+##' @param ... further arguments to be imposed to fitCatches (like smoothing parameter - number of knots for the gam)
 ##' @return 
 ##' @author Fer Arce
 ##' @export
@@ -20,4 +20,4 @@ projCatches <- function(Data, proj, ...) {
     pred$ucl <- round(pred$fit + 1.96*pred$se.fit, 1)
     pred$n.occ <- 1:nrow(pred)
     return(pred)
-}   
+}
