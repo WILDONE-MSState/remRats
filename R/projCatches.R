f58a6052a5bff1d95d4b005a0793203c0a7e1e58
Fer Arce <farcego@gmail.com>
2025-03-05 09:21:49 -0600

    reformatted output

--- R/projCatches.R
+++ R/projCatches.R
@@ -13,4 +13,11 @@ projCatches <- function(Data, proj, ...) {
     model <- fitCatches(Data, ...)
     test <- data.frame(n = 1:(nrow(Data$Nraw) + proj))
     pred <- predict(model, test,type = 'response', se = TRUE)
+    pred[[1]] <- floor(pred[[1]])
+    pred[[2]] <- round(pred[[2]], 1)
+    pred <- as.data.frame(pred)
+    pred$lcl <- round(pred$fit - 1.96*pred$se.fit, 1)
+    pred$ucl <- round(pred$fit + 1.96*pred$se.fit, 1)
+    pred$n.occ <- 1:nrow(pred)
+    return(pred)
 }   
