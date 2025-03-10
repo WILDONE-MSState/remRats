c8580b51dd529ebf1513279bd5be6a18f7f868cd
Fer Arce <farcego@gmail.com>
2025-03-10 09:04:33 -0500

    can be fitted to a subset of data

--- R/fitCatches.R
+++ R/fitCatches.R
@@ -5,20 +5,23 @@
 ##' cumulative number of animals removed, aimed to smoith a curve with
 ##' will be projected in order to evakluate current and future
 ##' performance of the removal experiment in regards the aims of the
-##' project. This function is not expected to be called by the end user
+##' project. This function is not expected to be called by the end
+##' user
 ##' @title Fitting a gam model to cumulative numbers of removed
 ##'     individuals.
 ##' @param Data an object of class fittedRemMLERec
 ##' @param k smoothing parameterfot the gam function - number of knots
 ##'     (It cannot be larger than n-1 occasions)
-##' @param length numbewr of occasions to fit the model 
+##' @param sub if prresent, subset of data to be be used for fitting
+##'     the gam model
+##' @param length numbewr of occasionsto fit the model
 ##' @return the object of a fitted gam model.
 ##' @author Fer Arce
 ##' @noRd
-fitCatches <- function(Data, k = 5, length = NULL) {
+fitCatches <- function(Data, k = 5, sub = NULL) {
     capts <- as.vector(Data$Nraw$Ncumu)
-    if (!is.null(length))
-        capts = capts[1:length]
+    if (!is.null(sub))
+        capts = capts[1:sub]
     n <- 1:length(capts)
     if (k >= (length(capts) - 1))
         k = length(capts) - 1
