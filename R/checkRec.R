d95e62c33ce9665d4a476d497cf47212cccde964
Fer Arce <farcego@gmail.com>
2025-07-25 09:58:11 -0500

    help added

--- R/checkRec.R
+++ R/checkRec.R
@@ -9,11 +9,11 @@
 checkRec <- function(rec){
     ## find weeird stuff
     ## 1 need to find - flag singular values
-    trends <- sign(diff(pob1))
+    trends <- sign(diff(rec))
     if (any(trends > 0))
         toIgnore <- which(trends > 0)
 ## if we dounf it
     if (exists('toIgnore'))
         ## we need to remove the weird value for definning the margins, not for plotting
-        margins <- list( y = res$ucl[order(res$ucl, decreasing = TRUE)][[2]])
+        margins <- list( y = rec$ucl[order(rec$ucl, decreasing = TRUE)][[2]])
 }
