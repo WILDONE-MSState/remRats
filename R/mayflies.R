4c8ba7c73a2920728e29c2934d65698bed8cf81a
Fer Arce <farcego@gmail.com>
2025-07-28 09:21:58 -0500

    whitefish datased help added

---
+++ R/mayflies.R
@@ -0,0 +1,11 @@
+#' mayflies dataset example
+#'
+#'A vector of succesive removals events of mayflies
+#'
+#' @format A numeric vector of length 5
+#' @source Otis, David L, Kenneth P. Burnham, Gary C. White, and David R. Anderson. “Statistical Inference from Capture Data on Closed Animal Populations.” Wildlife Monographs 62 (1978): 3–135.
+#' @examples
+#' data(mayflies)
+"myflies"
+
+
